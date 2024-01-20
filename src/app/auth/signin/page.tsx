2cff67f1508e59b66f6d6cbcaa2fbaf3bec584f4
Onildo Joao <onildojoaocosta@gmail.com>
2024-01-20 16:49:59 -0300

    etc

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 const SigninPage = ({ searchParams }: Props) => {
-  console.log({ searchParams });
 
   return (
     <main className="flex flex-col items-center justify-center p-5 h-[calc(100vh-65px)]">
